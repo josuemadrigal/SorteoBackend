938ab863550a0d52f8abcb3a219a15f347a4f141
nippyferreras <a.ferreras@nippy.la>
2024-06-25 10:28:04 -0400

    Update cors

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const { dbMySql } = require("./database/database");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://app.eduardespiritusanto.com",
+  optionsSuccessStatus: 200, // Algunas versiones antiguas de navegadores (IE11, varios SmartTVs) requieren el estatus 204 en lugar de 200
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static("public"));
 
